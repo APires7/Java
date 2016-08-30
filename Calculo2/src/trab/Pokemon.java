ed3332b5876927c4e9bc3f26e6935fb96dcae71e
eduardopac <eduardopacheco0306@gmail.com>
2016-08-30 18:43:32 -0300

    update 30_08

--- Calculo2/src/trab/Pokemon.java
+++ Calculo2/src/trab/Pokemon.java
@@ -4,14 +4,128 @@ import java.util.Random;
 
 public class Pokemon {
 	
-	Ponto pontoPoke = new Ponto();
-
-	public int gerarPokemon() {
-		Random random = new Random();
-		int min = 50;
-		int quantidade = random.nextInt(min) + min;
-		// System.out.println(quantidade);
-		return quantidade;
+	private String nome;
+	private int num;
+	private String tipo;
+	private double cordX
+	private double cordY;
+	private double cordZ;
+        
+        
+        public Pokemon{
+            nome = "";
+            num = 0;
+            tipo = "";
+            cordX = 0.0;
+            cordY = 0.0;
+            cordZ = 0.0;
+        }
+	
+	
+	private pokedex<Pokemon> = new List<Pokemon>;
+	
+	public gerarListaPoke(){
+            pokedex.add{"bulbasaur",1,Grama,0,0,0};
+            pokedex.add{"bulbasaur",2,Grama,0,0,0};
+            pokedex.add{"bulbasaur",3,Grama,0,0,0};
+            pokedex.add{"bulbasaur",4,Grama,0,0,0};
+            pokedex.add{"bulbasaur",5,Grama,0,0,0};
+            pokedex.add{"bulbasaur",6,Grama,0,0,0};
+            pokedex.add{"bulbasaur",7,Grama,0,0,0};
+            pokedex.add{"bulbasaur",8,Grama,0,0,0};
+            pokedex.add{"bulbasaur",9,Grama,0,0,0};
+            pokedex.add{"bulbasaur",10,Grama,0,0,0};
+            pokedex.add{"bulbasaur",11,Grama,0,0,0};
+            pokedex.add{"bulbasaur",12,Grama,0,0,0};
+            pokedex.add{"bulbasaur",13,Grama,0,0,0};
+            pokedex.add{"bulbasaur",14,Grama,0,0,0};
+            pokedex.add{"bulbasaur",15,Grama,0,0,0};
+            pokedex.add{"bulbasaur",16,Grama,0,0,0};
+            pokedex.add{"bulbasaur",17,Grama,0,0,0};
+            pokedex.add{"bulbasaur",18,Grama,0,0,0};
+            pokedex.add{"bulbasaur",19,Grama,0,0,0};
+            pokedex.add{"bulbasaur",20,Grama,0,0,0};
+            pokedex.add{"bulbasaur",21,Grama,0,0,0};
+            pokedex.add{"bulbasaur",22,Grama,0,0,0};
+            pokedex.add{"bulbasaur",23,Grama,0,0,0};
+            pokedex.add{"bulbasaur",24,Grama,0,0,0};
+            pokedex.add{"bulbasaur",25,Grama,0,0,0};
+            pokedex.add{"bulbasaur",26,Grama,0,0,0};
+            pokedex.add{"bulbasaur",27,Grama,0,0,0};
+            pokedex.add{"bulbasaur",28,Grama,0,0,0};
+            pokedex.add{"bulbasaur",29,Grama,0,0,0};
+            pokedex.add{"bulbasaur",30,Grama,0,0,0};
+            pokedex.add{"bulbasaur",31,Grama,0,0,0};
+            pokedex.add{"bulbasaur",32,Grama,0,0,0};
+            pokedex.add{"bulbasaur",33,Grama,0,0,0};
+            pokedex.add{"bulbasaur",34,Grama,0,0,0};
+            pokedex.add{"bulbasaur",35,Grama,0,0,0};
+            pokedex.add{"bulbasaur",36,Grama,0,0,0};
+            pokedex.add{"bulbasaur",37,Grama,0,0,0};
+            pokedex.add{"bulbasaur",38,Grama,0,0,0};
+            pokedex.add{"bulbasaur",39,Grama,0,0,0};
+            pokedex.add{"bulbasaur",40,Grama,0,0,0};
+            pokedex.add{"bulbasaur",41,Grama,0,0,0};
+            pokedex.add{"bulbasaur",42,Grama,0,0,0};
+            pokedex.add{"bulbasaur",43,Grama,0,0,0};
+            pokedex.add{"bulbasaur",44,Grama,0,0,0};
+            pokedex.add{"bulbasaur",45,Grama,0,0,0};
+            pokedex.add{"bulbasaur",46,Grama,0,0,0};
+            pokedex.add{"bulbasaur",47,Grama,0,0,0};
+            pokedex.add{"bulbasaur",48,Grama,0,0,0};
+            pokedex.add{"bulbasaur",49,Grama,0,0,0};
+            pokedex.add{"bulbasaur",50,Grama,0,0,0};
+            pokedex.add{"bulbasaur",51,Grama,0,0,0};
+            pokedex.add{"bulbasaur",52,Grama,0,0,0};
+            pokedex.add{"bulbasaur",53,Grama,0,0,0};
+            pokedex.add{"bulbasaur",54,Grama,0,0,0};
+            pokedex.add{"bulbasaur",55,Grama,0,0,0};
+            pokedex.add{"bulbasaur",56,Grama,0,0,0};
+            pokedex.add{"bulbasaur",57,Grama,0,0,0};
+            pokedex.add{"bulbasaur",58,Grama,0,0,0};
+            pokedex.add{"bulbasaur",59,Grama,0,0,0};
+            pokedex.add{"bulbasaur",60,Grama,0,0,0};
+            pokedex.add{"bulbasaur",61,Grama,0,0,0};
+            pokedex.add{"bulbasaur",62,Grama,0,0,0};
+            pokedex.add{"bulbasaur",63,Grama,0,0,0};
+            pokedex.add{"bulbasaur",64,Grama,0,0,0};
+            pokedex.add{"bulbasaur",65,Grama,0,0,0};
+            pokedex.add{"bulbasaur",66,Grama,0,0,0};
+            pokedex.add{"bulbasaur",67,Grama,0,0,0};
+            pokedex.add{"bulbasaur",68,Grama,0,0,0};
+            pokedex.add{"bulbasaur",69,Grama,0,0,0};
+            pokedex.add{"bulbasaur",70,Grama,0,0,0};
+            pokedex.add{"bulbasaur",71,Grama,0,0,0};
+            pokedex.add{"bulbasaur",72,Grama,0,0,0};
+            pokedex.add{"bulbasaur",73,Grama,0,0,0};
+            pokedex.add{"bulbasaur",74,Grama,0,0,0};
+            pokedex.add{"bulbasaur",75,Grama,0,0,0};
+            pokedex.add{"bulbasaur",76,Grama,0,0,0};
+            pokedex.add{"bulbasaur",77,Grama,0,0,0};
+            pokedex.add{"bulbasaur",78,Grama,0,0,0};
+            pokedex.add{"bulbasaur",79,Grama,0,0,0};
+            pokedex.add{"bulbasaur",80,Grama,0,0,0};
+            pokedex.add{"bulbasaur",81,Grama,0,0,0};
+            pokedex.add{"bulbasaur",82,Grama,0,0,0};
+            pokedex.add{"bulbasaur",83,Grama,0,0,0};
+            pokedex.add{"bulbasaur",84,Grama,0,0,0};
+            pokedex.add{"bulbasaur",85,Grama,0,0,0};
+            pokedex.add{"bulbasaur",86,Grama,0,0,0};
+            pokedex.add{"bulbasaur",87,Grama,0,0,0};
+            pokedex.add{"bulbasaur",88,Grama,0,0,0};
+            pokedex.add{"bulbasaur",89,Grama,0,0,0};
+            pokedex.add{"bulbasaur",90,Grama,0,0,0};
+            pokedex.add{"bulbasaur",91,Grama,0,0,0};
+            pokedex.add{"bulbasaur",92,Grama,0,0,0};
+            pokedex.add{"bulbasaur",93,Grama,0,0,0};
+            pokedex.add{"bulbasaur",94,Grama,0,0,0};
+            pokedex.add{"bulbasaur",95,Grama,0,0,0};
+            pokedex.add{"bulbasaur",96,Grama,0,0,0};
+            pokedex.add{"bulbasaur",97,Grama,0,0,0};
+            pokedex.add{"bulbasaur",98,Grama,0,0,0};
+            pokedex.add{"bulbasaur",99,Grama,0,0,0};
+            pokedex.add{"bulbasaur",100,Grama,0,0,0};
+            
 	}
 	
 }
--- Calculo2/src/trab/Ponto.java
+++ Calculo2/src/trab/Ponto.java
@@ -8,41 +8,71 @@ public class Ponto {
 	protected int areaY = 13000;
 	protected int areaZ = 4;
 
-	static int x;
-	static int y;
-	static int z;
+	static double x;
+	static double y;
+	static double z;
 
 	public Ponto() {
-		x = 0;
-		y = 0;
-		z = 0;
+		x = 0.0;
+		y = 0.0;
+		z = 0.0;
 	}
 
-	public Ponto(int a, int b, int c) {
+	public Ponto(double a, double b, double c) {
 		x = a;
 		y = b;
 		z = c;
 	}
 	
-	public void gerarPonto(int quantidade) {
+        public int gerarQuantPokemon() { //gera a quantidade de pokemons que iram aparecer na area
+		Random random = new Random();
+		int min = 50;
+		int quantidade = random.nextInt(min) + min;
+		// System.out.println(quantidade);
+		return quantidade;
+	}
+	
+	Pokemon poke = new Pokemon();
+	
+	List<Pokemon> pokem = poke.geraListaPoke();
+	
+	public void gerarPonto(int quantidade) {//gera as coordenadas dos pokemons colocando-os na lista
 		for (int i = 0; i < quantidade; i++) {
-			System.out.println("P" + i);
-
+	
 			Random randomX = new Random();
 			int eixoX = randomX.nextInt(13000);
-			System.out.println("X:" + eixoX);
+			pokem[i].add(cordX = eixoX);
+
 
 			Random randomY = new Random();
 			int eixoY = randomX.nextInt(13000);
-			System.out.println("Y:" + eixoY);
-
+			pokem[i].add(cordY = eixoY);
+			
 			Random randomZ = new Random();
 			int eixoZ = randomX.nextInt(4);
-			System.out.println("Z:" + eixoZ);
-			System.out.println("---------------");
+			pokem[i].add(cordZ = eixoZ);
+			
 			
 			verificaCoord(eixoX, eixoY, eixoZ);
+			CompararPontos();
 		}
+	}
+	
+	public void CompararPontos(){//É calculado a distancia entre dois pokemos
+	}
+	
+	public void mostrarLista(){//Sera mostrado a lista de pokemons que seram dispostos na area
+	
+                    for (int i = 0; i < quantidade; i++) {
+			System.out.println("Numero: "pokem[i].num);
+			System.out.println("Nome: " pokem[i].nome);
+			System.out.println("Tipo: "pokem[i].tipo);
+                        System.out.println("cordX: " + pokem[i].cordX);
+			System.out.println("cordY: " + pokem[i].cordY);
+                        System.out.println("cordZ:" + pokem[i].cordZ);
+			System.out.println("---------------");
+			
+                    }
 	}
 
 	void verificaCoord(int a, int b, int c) {
